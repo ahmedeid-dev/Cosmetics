9db1f9f6c8299188018fd5db9d1554b4059b858b
ahmedeid2260 <a.email2260@gmail.com>
2024-09-30 14:52:17 +0300

    generating modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './module/user/user.module';
+import { ProductModule } from './module/product/product.module';
+import { CategoryModule } from './module/category/category.module';
+import { SubcategoryModule } from './module/subcategory/subcategory.module';
+import { BrandModule } from './module/brand/brand.module';
+import { CartModule } from './module/cart/cart.module';
+import { OrderModule } from './module/order/order.module';
+import { CouponModule } from './module/coupon/coupon.module';
+import { ReviewModule } from './module/review/review.module';
+import { AuthModule } from './module/auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, ProductModule, CategoryModule, SubcategoryModule, BrandModule, CartModule, OrderModule, CouponModule, ReviewModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
